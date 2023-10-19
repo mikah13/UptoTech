4edd13b2a3ed488deabfd276d195a0bc58f62986
mikah13 <anhminhhoang13@gmail.com>
2023-10-18 18:25:54 -0700

    add: linkedom

--- src/app/api/scrapper/route.ts
+++ src/app/api/scrapper/route.ts
@@ -1,10 +1,27 @@
+import { DOMParser, parseHTML } from 'linkedom';
+
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const DEFAULT_SITE_URL = 'https://engineering.fb.com/';
+
   const siteURL = searchParams.get('siteURL') || DEFAULT_SITE_URL;
+  const metadata = searchParams.get('metadata');
 
   const res = await fetch(siteURL, {});
-  const content = await res.text();
+  const html = await res.text();
 
-  return Response.json({ content });
+  const {
+    // note, these are *not* globals
+    window,
+    document,
+    customElements,
+    HTMLElement,
+    Event,
+    CustomEvent,
+    // other exports ..
+  } = parseHTML(html);
+
+  console.log(document.querySelectorAll('.entry-title a').length);
+
+  return Response.json({ html });
 }
