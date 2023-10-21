740ea3d369e7a44e8073c522bca7039c8dfab604
mikah13 <anhminhhoang13@gmail.com>
2023-10-21 03:11:43 -0700

    create header file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,17 @@
 import Hero from '@/components/hero';
 import PostCards from '@/components/post-cards';
 import { PostContextProvider } from '@/components/post-provider';
+import { Card } from '@/components/ui/card';
 
 export default function Home() {
   return (
     <PostContextProvider>
-      <main className='flex flex-col items-center justify-between p-12'>
-        <Hero />
-        <PostCards />
+      <main className='relative flex flex-col items-center justify-between p-12'>
+        <div className='absolute inset-0  h-full -z-[10]  w-full bg-white bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]'></div>
+        <Card className='px-12'>
+          <Hero />
+          <PostCards />
+        </Card>
       </main>
     </PostContextProvider>
   );
