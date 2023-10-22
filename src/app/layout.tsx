f7c4df9473c021855fb6bcc3b91be30838ef61fa
mikah13 <anhminhhoang13@gmail.com>
2023-10-22 01:11:04 -0700

    update: add ograph and twitter to seo metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,10 +6,54 @@ import { ThemeProvider } from '@/components/theme-provider';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://uptotech.vercel.app/'),
   title: 'UptoTech - Exploring Tech with Industry Leaders',
+  icons: {
+    icon: '/favicon.ico',
+  },
   description:
     'Compile from various technical blogs into one central hub, saving you time and ensuring you updated with the industry leading companies in the world !',
-  
+  applicationName: 'UptoTech',
+  referrer: 'origin-when-cross-origin',
+  keywords: [
+    'UptoTech',
+    'Blog Post',
+    'Big Tech',
+    'Meta',
+    'Google',
+    'IBM',
+    'DropBox',
+    'Apple',
+  ],
+  authors: [{ name: 'Mike Hoang' }],
+  creator: 'Mike Hoang',
+  openGraph: {
+    title: 'UptoTech - Exploring Tech with Industry Leaders',
+    description:
+      'Compile from various technical blogs into one central hub, saving you time and ensuring you updated with the industry leading companies in the world !',
+    url: 'https://uptotech.vercel.app/',
+    images: [
+      {
+        url: 'https://uptotech.vercel.app/favicon.ico',
+        width: 128,
+        height: 128,
+        alt: 'UptoTech',
+      },
+    ],
+  },
+  twitter: {
+    title: 'UptoTech - Exploring Tech with Industry Leaders',
+    description:
+      'Compile from various technical blogs into one central hub, saving you time and ensuring you updated with the industry leading companies in the world !',
+    images: [
+      {
+        url: 'https://uptotech.vercel.app/avicon.ico',
+        width: 128,
+        height: 128,
+        alt: 'UptoTech',
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
