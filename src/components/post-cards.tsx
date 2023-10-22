be1200221906dc5a367425d793d0dd2d1dc04530
mikah13 <anhminhhoang13@gmail.com>
2023-10-22 01:14:36 -0700

    update: fix layout responsiveness for skeleton load

--- src/components/post-cards.tsx
+++ src/components/post-cards.tsx
@@ -40,7 +40,7 @@ const PostCards = (props: Props) => {
         </TabsList>
 
         {isLoading && (
-          <div className='grid grid-cols-3 gap-x-4 gap-y-8 text-left mt-2'>
+          <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3  gap-x-4 gap-y-8 text-left mt-2'>
             {new Array(12).fill('').map((e, i) => (
               <SkeletonCard key={i} />
             ))}
