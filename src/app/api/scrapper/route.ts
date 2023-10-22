3cfc027b990a2b68a0a992fc828e6b6b94c36559
mikah13 <anhminhhoang13@gmail.com>
2023-10-22 03:21:34 -0700

    fix: migrate from linkedom to cheerio

--- src/app/api/scrapper/route.ts
+++ src/app/api/scrapper/route.ts
@@ -1,6 +1,5 @@
 import { DATE_CONVERSION_FUNCTION, Post } from '@/lib/type';
-import { DOMParser } from 'linkedom';
-
+import * as cheerio from 'cheerio';
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const DEFAULT_SITE_URL = 'https://engineering.fb.com/';
@@ -17,36 +16,60 @@ export async function GET(request: Request) {
   const platform = searchParams.get('platform') || 'meta';
   const res = await fetch(siteURL, {});
   const html = await res.text();
-  const parser = new DOMParser();
-  const document = parser.parseFromString(html, 'text/html');
+  const $ = cheerio.load(html);
 
-  const posts: Post[] = Array.from(document.querySelectorAll(postSelector)).map(
-    (post) => {
-      const title =
-        post.querySelector(titleSelector)?.textContent?.trim() || 'Untited';
-      const link =
-        post.querySelector(linkSelector)?.getAttribute('href')?.trim() || '#';
+  // const parser = new DOMParser();
+  // const document = parser.parseFromString(html, 'text/html');
 
-      const thumbnail =
-        post.querySelector(thumbnailSelector)?.getAttribute('src')?.trim() ||
-        '';
-
-      let date = post.querySelector(dateSelector)?.textContent?.trim();
-      if (date) {
-        date =
-          DATE_CONVERSION_FUNCTION[
-            dateFunction as keyof typeof DATE_CONVERSION_FUNCTION
-          ](date);
-      }
-      const tags = Array.from(post.querySelectorAll(tagsSelector))
-        .map((e: any) => {
-          return e.innerText?.trim() || '';
-        })
-        .filter((e) => e !== '');
-
-      return { title, link, thumbnail, date, tags };
+  // const document = htmlparser2.parseDocument(htmlString);
+  let posts: Post[] = [];
+  let allPosts = $(postSelector);
+  allPosts.each(function (index, e) {
+    const title = $(e).find(titleSelector)?.first()?.text().trim() || 'Untited';
+    const link = $(e).find(linkSelector)?.first()?.attr('href')?.trim() || '#';
+    const thumbnail =
+      $(e).find(thumbnailSelector)?.first()?.attr('src')?.trim() || '';
+    let date = $(e).find(dateSelector)?.first()?.text()?.trim();
+    if (date) {
+      date =
+        DATE_CONVERSION_FUNCTION[
+          dateFunction as keyof typeof DATE_CONVERSION_FUNCTION
+        ](date);
     }
-  );
+    let tags: string[] = [];
+    const allTags = $(e).find(tagsSelector);
+    allTags.each(function (i, t) {
+      const tag = $(t).text().trim();
+      if (tag !== '') {
+        tags.push(tag);
+      }
+    });
+    posts.push({ title, link, thumbnail, date, tags });
+  });
+  // const posts: Post[] = Array.from($(postSelector)).map((post) => {
+  //   const title =
+  //     post.querySelector(titleSelector)?.textContent?.trim() || 'Untited';
+  //   const link =
+  //     post.querySelector(linkSelector)?.getAttribute('href')?.trim() || '#';
+
+  //   const thumbnail =
+  //     post.querySelector(thumbnailSelector)?.getAttribute('src')?.trim() || '';
+
+  //   let date = post.querySelector(dateSelector)?.textContent?.trim();
+  //   if (date) {
+  //     date =
+  //       DATE_CONVERSION_FUNCTION[
+  //         dateFunction as keyof typeof DATE_CONVERSION_FUNCTION
+  //       ](date);
+  //   }
+  //   const tags = Array.from(post.querySelectorAll(tagsSelector))
+  //     .map((e: any) => {
+  //       return e.innerText?.trim() || '';
+  //     })
+  //     .filter((e) => e !== '');
+
+  //   return { title, link, thumbnail, date, tags };
+  // });
 
   return Response.json({ platform, posts });
 }
