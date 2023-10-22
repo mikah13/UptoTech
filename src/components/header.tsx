b0ee70982146654cb47b8b00db3acb9e7f8d179b
mikah13 <anhminhhoang13@gmail.com>
2023-10-22 00:23:37 -0700

    update: finished footer

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { ModeToggle } from './mode-toggler';
+import Link from 'next/link';
 
 type Props = {};
 
 const Header = (props: Props) => {
   return (
     <div className='px-12 py-4 flex flex-row justify-between border-b '>
-      <div className='flex items-center font-semibold'>{'</>'} UptoTech</div>
+      <div className='flex items-center font-semibold'>
+        <Link href='/'>{'</>'} UptoTech</Link>
+      </div>
 
       <div>
         <ModeToggle />
