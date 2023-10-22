f3ddf8736f6a2eba78f653f8c75812fa16475657
mikah13 <anhminhhoang13@gmail.com>
2023-10-22 01:12:31 -0700

    add: endpoint to next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
+    env: {
+        BASE_URL: process.env.BASE_URL,
+    },
     images: {
         remotePatterns: [
             {
