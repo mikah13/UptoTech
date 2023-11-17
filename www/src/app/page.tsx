2c57dd8b16de4946f6ec3bb74e567689f97ece56
mikah13 <anhminhhoang13@gmail.com>
2023-11-16 21:25:21 -0800

    added custom 404 page

--- www/src/app/page.tsx
+++ www/src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
     <React.Suspense fallback={<div>Loading...</div>}>
       <main className='relative flex flex-col items-center justify-between p-2 lg:p-12'>
         <div className='absolute inset-0  h-full -z-[10]  w-full bg-white dark:bg-zinc-900 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]'></div>
-        <Card>
+        <Card className='w-full'>
           <CardHeader className='px-0 py-0'>
             <Header />
           </CardHeader>
